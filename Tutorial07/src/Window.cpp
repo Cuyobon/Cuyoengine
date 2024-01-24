5288d0eeaecaff1e43cfbeefc1113208463feb5c
Cuyobon <140874123+Cuyobon@users.noreply.github.com>
2024-01-24 17:34:12 -0600

    Update Window

--- Tutorial07/src/Window.cpp
+++ Tutorial07/src/Window.cpp
@@ -29,7 +29,7 @@ HRESULT Window::init(HINSTANCE hInstance, int nCmdShow, WNDPROC wndproc)
         return E_FAIL;
 
     // Create window
-    RECT rc = { 0, 0, 1500, 840 };
+    RECT rc = { 0, 0, 1000, 1000 };
     m_rect = rc;
     AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
     m_hWnd = CreateWindow("TutorialWindowClass",
